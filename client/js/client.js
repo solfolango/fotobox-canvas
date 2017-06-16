edbe15d2e243ec4f3b67753f80abe1f90b528eac
Sebastian Will <sebastian.will@tik.uni-stuttgart.de>
2017-06-16 17:05:15 +0200

    Draw border and dropshadow on images

--- client/js/client.js
+++ client/js/client.js
@@ -10,6 +10,10 @@ context.lineTo(3456/2,2304);
 context.moveTo(0,2304/2);
 context.lineTo(3456, 2304/2);
 context.stroke();
+
+
+context.shadowColor = 'black';
+context.shadowBlur = 30;
 /*
 for (var i = 0; i < 3456; i+=100) {
     
@@ -27,15 +31,22 @@ context.stroke();
 */
 
 var drawImage = function(img, spec) {
-    context.strokeStyle="#FF0000";
-    
+    context.strokeStyle="#FFFFFF";
+    context.lineWidth = 50;
+    var border = Math.round(spec.height / 15);
+
     console.log(img);
     
     console.log('Received spec'); console.log(spec);
-    var centerX = Math.round(0.5 * spec.width);
-    var centerY = Math.round(0.5 * spec.height);
+    var centerX = Math.round(0.5 * (spec.width + border));
+    var centerY = Math.round(0.5 * (spec.height + border));
 
     console.log("center: " + centerX + "/" + centerY);
+
+    
+    var shadowOffset = 15;
+    context.shadowOffsetX = shadowOffset;
+    context.shadowOffsetY = shadowOffset;
 
     // translate to the middle of the image
     context.translate(spec.left + centerX, spec.top + centerY);
@@ -43,7 +54,22 @@ var drawImage = function(img, spec) {
     context.rotate((spec.rotate + 180) * Math.PI / 180);
     // translate to the top/left of the image
     context.translate(-1 * centerX, -1 * centerY);// 0.5*spec.width, -0.5*spec.height);
+    
+    
+    var tCanvas = document.createElement('canvas');
+    tCanvas.width = spec.width + 2*border; tCanvas.height = spec.height + 2*border;
+    var tContext = tCanvas.getContext('2d');
+    tContext.fillStyle = '#FFFFFF';
+    tContext.fillRect(0, 0, spec.width + 2*border, spec.height + 2*border);
+    tContext.drawImage(img, border, border, spec.width, spec.height);
+
+    context.drawImage(tCanvas, 0, 0);
+    
+/*
     context.drawImage(img, 0, 0, spec.width, spec.height);
+    context.rect(0, 0, spec.width, spec.height);
+    */
+    
     context.setTransform(1, 0, 0, 1, 0, 0);
     socket.emit('drew-image');
 }
